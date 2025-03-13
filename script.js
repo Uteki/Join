7cb99241b73319e8527368eaf1f8e5798760b1b7
lucas <lucas.hamacher@ma-versand.de>
2025-03-13 15:57:07 +0100

    add header bar

---
+++ script.js
@@ -0,0 +1,6 @@
+const logoutMenu = document.getElementById('logout-menu')
+const userInitials = document.getElementById('user-initials-container')
+
+function toggleLogoutMenu(){
+    logoutMenu.classList.toggle('d-none')
+}
