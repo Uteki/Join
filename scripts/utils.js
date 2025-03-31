be3af9116a6a9374cb6eb38a24d59c734af0ca66
lucas <lucashamacher01@gmail.com>
2025-03-31 07:39:15 +0200

    turn initials to uppercase

--- scripts/utils.js
+++ scripts/utils.js
@@ -32,7 +32,7 @@ function getContactInitials(contactName){
         initials += name.slice(abbr + 1, abbr + 2);
     }
 
-    return initials;
+    return initials.toUpperCase();
 }
 
 function toggleVisibility(id){
