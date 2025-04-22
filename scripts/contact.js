0bfb60a6930b2a7692fd97b8f0b4d6243f37a0ce
lucas <lucashamacher01@gmail.com>
2025-04-22 12:48:27 +0200

    add JS Documentation

--- scripts/contact.js
+++ scripts/contact.js
@@ -6,6 +6,9 @@ const sidebar = document.getElementById("con-sidebar");
 
 const dropdownMenu = document.querySelector(".dd-menu");
 
+/**
+ * Loads and renders menu list of all of the contatcs.
+ */
 async function renderContact() {
     let index;
     await init(); windowContact();
@@ -20,6 +23,10 @@ async function renderContact() {
     }
 }
 
+/**
+ * Display a contact based of a given ID.
+ * @param {number} id 
+ */
 async function displayContact(id) {
     let contact = contactList.find(el => el.id === id); removeActiveContact()
     document.getElementById(`contact${id}`).classList.add("contact-active");
@@ -35,6 +42,11 @@ async function displayContact(id) {
     }
 }
 
+/**
+ * Turns the name of contact into his initals.
+ * @param {*} contact 
+ * @returns {string}
+ */
 async function getIni(contact) {
     let name = contact.trim();
     let abbr = name.indexOf(" ");
@@ -45,6 +57,9 @@ async function getIni(contact) {
     } return initials;
 }
 
+/**
+ * Show contact sidebar.
+ */
 function windowContact() {
     if (window.innerWidth <= 670) {
         document.getElementById("con-display").classList.add("d-none");
@@ -52,6 +67,9 @@ function windowContact() {
     }
 }
 
+/**
+ * Remove active contact styling of a contact element.
+ */
 function removeActiveContact() {
     document.querySelectorAll("#con-sidebar output button").forEach((button) => {
         button.classList.remove("contact-active");
